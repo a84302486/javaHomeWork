c605e01cbcb252f804d2a3c96f0d3a39014342c4
a84302486 <a84302486@gmail.com>
2017-01-06 13:45:12 +0800

    1/6

--- src/JDBCDB/Celebrity/celebrityMain.java
+++ src/JDBCDB/Celebrity/celebrityMain.java
@@ -7,9 +7,9 @@ public class celebrityMain {
 	public static void main(String[] args) throws IOException {
 		celebrityDAO dao = new celebrityDAO();
 		dao.createTables();
-		celebrityUtil.initCelebrity("http://www.space-fox.com/wallpapers/celebsm/vin-diesel/vin_diesel_1.jpg",9);
-		celebrityUtil.initCelebrity("http://www.space-fox.com/wallpapers/celebs/penelope-cruz/penelope_cruz_1.jpg",64);
-		celebrityUtil.initCelebrity("http://www.space-fox.com/wallpapers/celebsm/tom-cruise/tom_cruise_1.jpg",10);
+		celebrityUtil.initCelebrity("http://www.space-fox.com/wallpapers/celebsm/vin-diesel/vin_diesel_1.jpg");
+		celebrityUtil.initCelebrity("http://www.space-fox.com/wallpapers/celebs/penelope-cruz/penelope_cruz_1.jpg");
+		celebrityUtil.initCelebrity("http://www.space-fox.com/wallpapers/celebsm/tom-cruise/tom_cruise_1.jpg");
 	}
 
 }
--- src/JDBCDB/Celebrity/celebrityUtil.java
+++ src/JDBCDB/Celebrity/celebrityUtil.java
@@ -4,34 +4,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class celebrityUtil {
-	public static void initCelebrity(String url, int count) throws IOException{
+	public static void initCelebrity(String url) throws IOException{
 		
 		celebrityDAO dao = new celebrityDAO();
 		String urlHead = url.substring(0,url.lastIndexOf("_")+1);
 		String urlEnd = url.substring(url.lastIndexOf("."));
-			for (int x=1;x <= count;x++ ) {
-			HttpURLConnection connection = HttpToSQL(url = urlHead + x + urlEnd);
-			  String[] sa = url.split("/");
-			  String celeName  = sa[5].trim();
-			  String gender	 = sa[4].trim();
-			  if(gender.equals("celebsm")){
-				  gender = "M";
-			  }else{
-				  gender = "F";
-			  }
-			  String filename  = sa[6].trim();
-			  int filesize = connection.getContentLength();
-			  String timesave  = celebrityBean.getDate();
-			  InputStream picture = connection.getInputStream();
-			  System.out.println("明星"+celeName+"第"+x+"張圖片讀取成功");
+			try{
+				int x = 1;
+				while (true){
+				HttpURLConnection connection = HttpToSQL(url = urlHead + x + urlEnd);
+				  String[] sa = url.split("/");
+				  String celeName  = sa[5].trim();
+				  String gender	 = sa[4].trim();
+				  if(gender.equals("celebsm")){
+					  gender = "M";
+				  }else{
+					  gender = "F";
+				  }
+				  String filename  = sa[6].trim();
+				  int filesize = connection.getContentLength();
+				  String timesave  = celebrityBean.getDate();
+				  InputStream picture = connection.getInputStream();
+				  System.out.println("明星"+celeName+"第"+x+"張圖片讀取成功");
 
-			  celebrityBean cd = new celebrityBean (celeName, gender, filename, filesize, 
-					  timesave, picture);
-			  dao.insert(cd);
+				  celebrityBean cd = new celebrityBean (celeName, gender, filename, filesize, 
+						  timesave, picture);
+				  dao.insert(cd);
+				  x++;
+				}
+			}catch(IOException e){
+				System.out.println("已無圖檔");
 			}
 	}
 	public static HttpURLConnection HttpToSQL(String setUrl) throws IOException {
@@ -39,4 +43,5 @@ public class celebrityUtil {
 		HttpURLConnection connection =  (HttpURLConnection) getUrl.openConnection();
 		return connection;
 	}
+	
 }
